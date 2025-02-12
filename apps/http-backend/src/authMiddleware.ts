ae24a6dce7f3a7765491c2857a9715de4e116dae
Salman <salmannajah123@gmail.com>
2025-02-12 19:30:10 +0530

    Backend Checkpoint 2

---
+++ apps/http-backend/src/authMiddleware.ts
@@ -0,0 +1,19 @@
+import { JWT_SECRET } from "./config"
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers["authorization"] ?? "";
+
+    const decoded = jwt.verify(authHeader, JWT_SECRET) as JwtPayload;
+
+    if (decoded) {
+        //@ts-ignore
+        req.userId = decoded.userId;
+        next(); 
+    } else {
+        res.status(403).json({
+            message: "Invalid token"
+        });
+    }
+}
--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,60 @@
 import express from "express"
+import bcrypt from "bcrypt"
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { JWT_SECRET } from "./config"
+import { authMiddleware } from "./authMiddleware";
+import * as src from "@repo/db/client";
+
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.post('/api/v1/signup', async (req, res) => {
+    const { username, password } = req.body;
+
+    //Hashing the password
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    const user = await src.client.user.create({
+        data: {
+            username: username,
+            password: hashedPassword
+        }
+    })
     res.json({
-        message: "Hello express App"
+        message: "Signup successful",
+        id: user.id
+    });
+})
+
+app.post('/api/v1/signin', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await src.client.user.findUnique({
+        where: {
+            username
+        }
+    })
+
+    const userId = user?.id;
+
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.json({
+        message: "Signed in successfully",
+        userId: userId,
+        token: token
     })
 })
 
-app.listen(3000);
+app.get('/api/v1/room',authMiddleware, (req, res) => {
+
+    //DB Call
+    res.json({
+        roomID: 12345
+    })
+})
+
+app.listen(3001);
